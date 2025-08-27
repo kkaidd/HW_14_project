c9d8fbb8a850157ca75b70809ece682d51547ea3
KMsulimanov <kaborzid@gmail.com>
2025-08-27 23:48:45 +0300

    replace lambda steps with pObject methods and clean up unused code

--- src/test/java/pages/HhHomePage.java
+++ src/test/java/pages/HhHomePage.java
@@ -2,6 +2,7 @@ package pages;
 
 import com.codeborne.selenide.SelenideElement;
 import com.codeborne.selenide.WebDriverRunner;
+import io.qameta.allure.Step;
 import org.openqa.selenium.JavascriptExecutor;
 
 import static com.codeborne.selenide.Condition.*;
@@ -10,56 +11,47 @@ import static com.codeborne.selenide.Selenide.*;
 public class HhHomePage {
 
     private SelenideElement jobSearchInput = $("[data-qa='search-input']"),
-            submitButton = $("[data-qa='search-button']");
+            submitButton = $("[data-qa='search-button']"),
+            removeBanner = $(".bloko-modal-close-button"),
+            mainPageHeader = $("[data-qa='main-page-anonymous-header']"), // Добавлен элемент
+            searchResults = $("[data-qa='vacancy-serp__results']"); // Добавлен элемент
 
     public HhHomePage openPage() {
         open("/");
         return this;
     }
 
-    public HhHomePage removeBanner() {
-        executeJavaScript("$('#fixedban').remove()");
-        executeJavaScript("$('footer').remove()");
+    @Step("Проверить заголовок главной страницы")
+    public HhHomePage verifyMainPageHeader() {
+        mainPageHeader.shouldHave(text("Работа найдётся для каждого"));
         return this;
     }
 
-    public HhHomePage jobSearchInput(String value) {
+    @Step("Ввести значение '{value}' в поле поиска")
+    public HhHomePage setJobSearchInput(String value) {
         jobSearchInput.shouldBe(visible).setValue(value);
         return this;
     }
 
-    public HhHomePage submitButton() {
+    @Step("Нажать кнопку поиска")
+    public HhHomePage clickSubmitButton() {
         submitButton.click();
         return this;
     }
 
-    public HhHomePage removeHhBanner() {
-        executeJavaScript(
-                "var banner = document.querySelector('.bloko-modal-container_visible');" +
-                        "if (banner) banner.remove();" +
-                        "var footer = document.querySelector('footer');" +
-                        "if (footer) footer.remove();"
-        );
-        executeJavaScript(
-                "var tooltip = document.querySelector('[data-qa=\"tooltip\"]');" +
-                        "if (tooltip) tooltip.remove();" +
-                        "var regionTooltip = document.querySelector('.magritte-tooltip___Hxyh2_10-0-6');" +
-                        "if (regionTooltip) regionTooltip.remove();"
-        );
-        executeJavaScript(
-                "var overlay = document.querySelector('.bloko-modal-overlay');" +
-                        "if (overlay) overlay.remove();"
-        );
+    @Step("Закрыть баннер")
+    public HhHomePage closeBanner() {
+        try {
+            removeBanner.shouldBe(visible).click();
+        } catch (Exception e) {
+
+        }
         return this;
     }
 
-    public void verifySearchResults() {
-        $("[data-qa='vacancy-serp__results']").shouldBe(visible);
-        $("[data-qa='vacancy-serp__results']").shouldHave(text("QA"));
-    }
-
-    private void executeJavaScript(String script) {
-        JavascriptExecutor js = (JavascriptExecutor) WebDriverRunner.getWebDriver();
-        js.executeScript(script);
+    @Step("Проверить результаты поиска")
+    public HhHomePage verifySearchResults() {
+        searchResults.shouldBe(visible).shouldHave(text("QA"));
+        return this;
     }
 }
--- src/test/java/tests/HhSearchTest.java
+++ src/test/java/tests/HhSearchTest.java
@@ -4,12 +4,6 @@ import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import pages.HhHomePage;
 
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.*;
-import static io.qameta.allure.Allure.step;
-
 public class HhSearchTest extends TestBase {
 
     HhHomePage hhHomePage = new HhHomePage();
@@ -17,24 +11,11 @@ public class HhSearchTest extends TestBase {
     @Test
     @Tag("simple")
     void searchQaVacancies() {
-        step("Открыть главную страницу HeadHunter", () -> {
-            hhHomePage.openPage();
-            $("[data-qa='main-page-anonymous-header']").shouldHave(text("Работа найдётся для каждого"));
-        });
-
-        step("Выполнить поиск вакансий по запросу 'QA'", () -> {
-            hhHomePage.jobSearchInput("QA")
-                    .submitButton();
-        });
-
-        step("Проверить, что отобразились результаты поиска", () -> {
-            hhHomePage.removeHhBanner().verifySearchResults();
-        });
+        hhHomePage.openPage()
+                .verifyMainPageHeader()
+                .setJobSearchInput("QA")
+                .clickSubmitButton()
+                .closeBanner()
+                .verifySearchResults();
     }
 }
-
-//    @Test
-//    @Tag("simple")
-//    void filterByExperience() {
-//
-//    }
