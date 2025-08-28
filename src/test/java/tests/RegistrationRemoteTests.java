ed55bea8396501f565a8cda9c33061be521c582d
KMsulimanov <kaborzid@gmail.com>
2025-08-28 03:58:26 +0300

    add screenshots

---
+++ src/test/java/tests/RegistrationRemoteTests.java
@@ -0,0 +1,44 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.Map;
+
+import static com.codeborne.selenide.Condition.appear;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+import static io.qameta.allure.Allure.step;
+
+public class RegistrationRemoteTests extends TestBase{
+
+    @Test
+    @Tag("simple")
+    void successfulRegistrationTest() {
+        step("Open homePage", () -> {
+            open("/");
+            $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
+
+            executeJavaScript("$('#fixedban').remove()");
+            executeJavaScript("$('footer').remove()");
+        });
+        step("Fill form", () -> {
+            $("#firstName").setValue("Alex").submit();
+        });
+
+        step("Verify results", () -> {
+            $(".modal-dialog").should(appear);
+            $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+            $(".table-responsive").shouldHave(text("Alex"), text("Egorov"),
+                    text("alex@egorov.com"), text("1234567890"));
+        });
+    }
+}
